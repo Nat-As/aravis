d7c963d89e0613302d2cba82c2f66fba8b2ac6f8
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 16:22:16 -0400

    Create deps.sh

---
+++ deps.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y build-essential meson ninja-build pkg-config \
+    libglib2.0-dev libxml2-dev zlib1g-dev libusb-1.0-0-dev \
+    gobject-introspection libgirepository1.0-dev \
+    libgtk-3-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+    libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
