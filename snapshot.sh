e5f2889f571607ddc3b743b501bd7973f35bf59c
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 18:42:53 -0400

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,1 +1,2 @@
-gst-launch-1.0 aravissrc num-buffers=1 ! videoconvert ! jpegenc ! filesink location=test.jpg
+# Take a single JPEG Image and save it to test.jpg
+timeout 3 gst-launch-1.0 aravissrc num-buffers=1 ! video/x-raw,width=1280,height=1024 ! videoconvert ! jpegenc ! filesink location=test.jpg
