f8f3a5e0b0a693bb04803e36dfde401a80e4b8df
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 16:34:46 -0400

    export path

--- deps.sh
+++ deps.sh
@@ -1,6 +1,8 @@
 sudo apt update
 #installs to /home/alakai_user/.local/bin/meson
 pip3 install --user meson ninja
+#move local install to path to compile with meson
+export PATH="$HOME/.local/bin:$PATH"
 sudo apt install -y build-essential ninja-build pkg-config \
     libglib2.0-dev libxml2-dev zlib1g-dev libusb-1.0-0-dev \
     gobject-introspection libgirepository1.0-dev \
