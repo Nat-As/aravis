974fcf045eef7036b32dea043100e5065f13375c
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 17:58:23 -0400

    Update support.sh

--- support.sh
+++ support.sh
@@ -1,2 +1,4 @@
-# Run ARV Windowed
-GST_PLUGIN_FEATURE_RANK="amlvsink:0,amlhalasink:0" arv-viewer
+arv-tool-0.10 control PixelFormat=Mono8 # Mono8
+arv-tool-0.10 control SensorTestPattern=On # Send test pattern
+arv-tool-0.10 control  # confirm camera is detected
+arv-tool-0.10 stream --filename=frame  # writes raw frames to disk, no video sink involved
