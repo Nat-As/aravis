f557e6a2b5ca9f710a3f570773a28248dabcb886
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 18:06:12 -0400

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,8 +1,1 @@
-gst-inspect-1.0 aravissrc
-mkdir -p kinetic_test
-gst-launch-1.0 -e \
-  aravissrc num-buffers=100 \
-    features="OutputDepth=Mono8 PixelFormat=Mono8 TestPattern=Off SensorTestPattern=On AGC=AGCON" \
-  ! video/x-raw,format=GRAY8 \
-  ! pngenc \
-  ! multifilesink location="kinetic_test/frame_%05d.png"
+gst-launch-1.0 aravissrc num-buffers=1 ! videoconvert ! jpegenc ! filesink location=test.jpg
