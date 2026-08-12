5744821ea8382f2825b18934522c3fc8eba1aa51
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 18:44:34 -0400

    Update and rename support.sh to setup-iNocturn.sh

--- support.sh
+++
@@ -1,4 +0,0 @@
-arv-tool-0.10 control PixelFormat=Mono8 # Mono8
-arv-tool-0.10 control SensorTestPattern=On # Send test pattern
-arv-tool-0.10 control  # confirm camera is detected
-arv-tool-0.10 stream --filename=frame  # writes raw frames to disk, no video sink involved
