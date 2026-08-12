d4dfc8b7f012eed4174fc599d3419c232273535e
James Andrews <jandrews7348@floridapoly.edu>
2026-08-12 16:33:49 -0400

    move meson to pip install

--- deps.sh
+++ deps.sh
@@ -1,5 +1,7 @@
 sudo apt update
-sudo apt install -y build-essential meson ninja-build pkg-config \
+#installs to /home/alakai_user/.local/bin/meson
+pip3 install --user meson ninja
+sudo apt install -y build-essential ninja-build pkg-config \
     libglib2.0-dev libxml2-dev zlib1g-dev libusb-1.0-0-dev \
     gobject-introspection libgirepository1.0-dev \
     libgtk-3-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
