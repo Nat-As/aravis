2d115112581c82251055407e5b814abc96f38810
James Andrews <jandrews7348@floridapoly.edu>
2026-08-13 10:50:48 -0400

    Update setup-iNocturn.sh

--- setup-iNocturn.sh
+++ setup-iNocturn.sh
@@ -1,5 +1,5 @@
 # Set up iNocturn config registers
 arv-tool-0.10 control  # confirm camera is detected
-arv-tool-0.10 control PixelFormat=Mono8 # Mono8
+arv-tool-0.10 control PixelFormat=Mono10
 arv-tool-0.10 control SensorTestPattern=On # Send test pattern
 arv-tool-0.10 control Width=1280 Height=1024
